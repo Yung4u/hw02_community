15feaca1c64613322523b8716c432187e6cb6958
Орлов Сергей <jokeblablo@gmail.com>
2023-01-12 23:16:12 +0300

    reupload

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200, verbose_name="название группы")
-    slug = models.SlugField(unique=True, verbose_name="слаг группы")
+    slug = models.SlugField(unique=True, verbose_name="ссылка группы")
     description = models.TextField(verbose_name="текст группы")
 
     def __str__(self):
